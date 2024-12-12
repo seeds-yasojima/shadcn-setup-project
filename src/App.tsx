4e74b0a0039695862af6d743a2c39b15974947d1
fumiya yasojima <yasojima@seeds-std.co.jp>
2024-12-12 15:23:09 +0900

    レイアウト調整

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,22 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <header className="bg-slate-100 py-2">
+        <div className="container mx-auto">
+          <h1 className="font-semibold">Vite + React</h1>
+        </div>
+      </header>
+
+      <main>
+        <div className="container mx-auto mt-6">
+          <div>
+            <button onClick={() => setCount((count) => count + 1)}>
+              count up
+            </button>
+          </div>
+          <div className="font-bold">{count}</div>
+        </div>
+      </main>
     </>
   );
 }
